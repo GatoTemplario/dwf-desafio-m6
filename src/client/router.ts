5019b47670bfae4eaaf4217b31179dd31bd48752
Eleri <n.erik.jak@gmail.com>
2023-01-23 08:54:51 -0300

    cambios menores

--- src/client/router.ts
+++ src/client/router.ts
@@ -3,20 +3,18 @@ import {Router} from "@vaadin/router"
 const router = new Router(document.querySelector(".root"))
 
 router.setRoutes([
-    {path: '/welcome' , component: 'welcome-page'},
-    {path: '/new-room' , component: 'newroom-page'},
-    {path: '/existent-room' , component: 'existentroom-page'},
+    {path: '/welcome'             , component: 'welcome-page'},
+    {path: '/new-room'            , component: 'newroom-page'},
+    {path: '/existent-room'       , component: 'existentroom-page'},
     {path: '/existent-room-error' , component: 'existentroom-error-page'},
-    {path: '/codigo' , component: 'codigo-page'},
-    {path: '/start' , component: 'start-page'},
-    {path: "/gameroom" , component: "gameroom-page"},
-    {path: "/result" , component: "result-page"},
+    {path: '/codigo'              , component: 'codigo-page'},
+    {path: '/start'               , component: 'start-page'},
+    {path: "/gameroom"            , component: "gameroom-page"},
+    {path: "/result"              , component: "result-page"},
 ])
 
 // SI FUNCIONA, HABRA QUE AGREGAR LUEGO PARA PREGUNTAR CUANDO ESTE DEPLOYADO ("HEROKUAPP.DESAFIO-MOD-8/")
 if(location.pathname == "/"){
-    console.log("estas aqui");
-    
     Router.go("/welcome")
 }
 
